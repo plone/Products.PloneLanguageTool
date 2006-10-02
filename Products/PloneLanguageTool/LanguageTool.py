04735d8922ba1fd7858a3b3daeaa60e48960b500
Hanno Schlichting <hanno@hannosch.eu>
2006-10-02 21:19:45 +0000

    Pyflaked! Removed some more obsolete import statements.
    svn path=/PloneLanguageTool/branches/plip174-reusable-i18n/; revision=31304

--- Products/PloneLanguageTool/LanguageTool.py
+++ Products/PloneLanguageTool/LanguageTool.py
@@ -1,9 +1,6 @@
 from AccessControl import ClassSecurityInfo
 from Globals import InitializeClass
 from OFS.SimpleItem import SimpleItem
-from Products.CMFCore.Expression import Expression
-from Products.CMFCore.ActionInformation import ActionInformation
-from Products.CMFCore.ActionProviderBase import ActionProviderBase
 from Products.CMFCore.permissions import ManagePortal
 from Products.CMFCore.permissions import View
 from Products.CMFCore.utils import UniqueObject, getToolByName
