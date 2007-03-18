55cad3b8e31c5af05111c4868a4513ef44dc2bec
Hanno Schlichting <hanno@hannosch.eu>
2007-03-18 20:06:34 +0000

    Added GS extension profile for product installation.
    svn path=/PloneLanguageTool/trunk/; revision=39373

--- Products/PloneLanguageTool/__init__.py
+++ Products/PloneLanguageTool/__init__.py
@@ -1,15 +1,13 @@
-from Products.CMFCore.DirectoryView import registerDirectory
 from Products.CMFCore.utils import ToolInit
-from Products.PloneLanguageTool.LanguageTool import LanguageTool
+from Products.PloneLanguageTool import LanguageTool
 
 ADD_CONTENT_PREMISSIONS = 'Manage Portal'
 lang_globals = globals()
-registerDirectory('skins', lang_globals)
 
 PKG_NAME = 'PloneLanguageTool'
 
 def initialize(context):
     ToolInit('Plone Language Tool',
-             tools=(LanguageTool,),
+             tools=(LanguageTool.LanguageTool,),
              icon='tool.gif',
     ).initialize(context)
