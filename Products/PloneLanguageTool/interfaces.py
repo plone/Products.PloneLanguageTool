9e6b1698cd1823463ff0e4a61b12cafa8c60c26e
Hanno Schlichting <hanno@hannosch.eu>
2008-04-11 21:06:37 +0000

    Hide the deprecation warning about the moved ITranslatable interface for our own code.
    svn path=/PloneLanguageTool/trunk/; revision=62532

--- Products/PloneLanguageTool/interfaces.py
+++ Products/PloneLanguageTool/interfaces.py
@@ -1,11 +1,13 @@
 from zope.interface import Interface
 
 # BBB Plone 4.0
+from zope.deprecation import __show__
+__show__.off()
 try:
     from Products.LinguaPlone.interfaces import ITranslatable
 except ImportError:
     from Products.CMFPlone.interfaces.Translatable import ITranslatable
-
+__show__.on()
 
 class ILanguageTool(Interface):
     """Marker interface for the portal_languages tool.
