65fb175eb15bd4cc987b95763735f4fe7486b723
Stefan H. Holek <stefan@epy.co.at>
2008-06-27 01:27:53 +0000

    Fix awkward method name.
    svn path=/PloneLanguageTool/branches/shh-2.0-subdomain-negotiator/; revision=67341

--- Products/PloneLanguageTool/LanguageTool.py
+++ Products/PloneLanguageTool/LanguageTool.py
@@ -522,8 +522,8 @@ class LanguageTool(UniqueObject, SimpleItem):
         """Returns the name for a country code."""
         return self.getAvailableCountries().get(countryCode, countryCode)
 
-    security.declareProtected(View, 'isSelectorAvailable')
-    def isSelectorAvailable(self):
+    security.declareProtected(View, 'showSelector')
+    def showSelector(self):
         """Returns True if the selector viewlet should be shown."""
         return bool(self.use_cookie_negotiation or self.always_show_selector)
 
