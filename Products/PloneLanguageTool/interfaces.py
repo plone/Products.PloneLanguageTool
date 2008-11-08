3e89712d3d09bab37d9fb2da84386d4f03f8417e
Hanno Schlichting <hanno@hannosch.eu>
2008-11-08 20:47:54 +0000

    The ITranslatable interface is gone from CMFPlone. Provide our own dummy version if LinguaPlone is not installed.
    svn path=/PloneLanguageTool/trunk/; revision=75310

--- Products/PloneLanguageTool/interfaces.py
+++ Products/PloneLanguageTool/interfaces.py
@@ -1,13 +1,10 @@
 from zope.interface import Interface
 
-# BBB Plone 4.0
-from zope.deprecation import __show__
-__show__.off()
 try:
     from Products.LinguaPlone.interfaces import ITranslatable
 except ImportError:
-    from Products.CMFPlone.interfaces.Translatable import ITranslatable
-__show__.on()
+    class ITranslatable(Interface):
+        pass
 
 class ILanguageTool(Interface):
     """Marker interface for the portal_languages tool.
